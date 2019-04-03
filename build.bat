887a7a687c0d68d6a193844db8db4d072d9350c7
Gordon Rudman <gordonrudman@gmail.com>
2019-04-03 20:48:43 +0100

    1

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-SET %KOTLIN_HOME%="D:\a\1\s\tools\lib"
+SET KOTLIN_HOME="D:\a\1\s\tools\kotlinc"
+REM SET KOTLIN_HOME="C:\ExperimentingWithKotlin\tools\kotlinc"
+SET PATH=%PATH%;%KOTLIN_HOME%\bin
 
-tools\kotlinc\bin\kotlinc src\app.kt -d out
-pause
+kotlinc src\app.kt -d out 
